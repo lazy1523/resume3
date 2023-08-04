8960c1cfe85457ca68205bfd0cbb6d9a62dfe699
zisu <lazy1523@outlook.com>
2023-08-05 02:37:22 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,32 +2,13 @@ import Layout from '@/components/sophon/layout/layout'
 import Loading from '@/components/sophon/loading/loading'
 import { Toaster } from '@/components/ui/toaster'
 import '@/src/styles/globals.css'
-import { userState } from '@/store/globalState'
+
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+
 import { RecoilRoot, useRecoilState } from 'recoil'
 export default function App({ Component, pageProps }: AppProps) {
 
   const ComponentWithUser = ({ Component, pageProps }: AppProps) => {
-    const router = useRouter();
-    const [, setUser] = useRecoilState(userState);
-
-    const accessibleRoutesWhenNotLoggedIn = ['/auth/login', '/auth/forgot', '/auth/register'];
-
-    useEffect(() => {
-      const storedUserData = localStorage.getItem('user');
-      if (storedUserData) {
-        setUser(JSON.parse(storedUserData));
-      } else if (!accessibleRoutesWhenNotLoggedIn.includes(router.pathname)) {
-        router.push('/auth/login');
-      }
-    }, [router, setUser]);
-
-    if (accessibleRoutesWhenNotLoggedIn.includes(router.pathname)) {
-      return <Component {...pageProps} />
-    }
-
     return (
       <Layout>
         <Component {...pageProps} />
